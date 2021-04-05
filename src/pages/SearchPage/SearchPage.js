c1c9e3f5f399dcf8c656246b94283b70e6ce9881
Min Gee Suh <suh@student.unimelb.edu.au>
2021-04-05 17:43:04 +0900

    navbar height change

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -4,17 +4,21 @@ import Input from "components/Input/Input";
 import PageContainer from "containers/PageContainer/PageContainer.styled";
 import useDetectViewport from "hooks/useDetectViewport";
 import { pageEffect } from "styles/motions/variants";
+import useDatabase from "hooks/useDatabase";
 
 /* -------------------------------------------------------------------------- */
 
 export default function SearchPage() {
   const { isMobile } = useDetectViewport();
+
+  const { findPost } = useDatabase("posts");
+  console.log(findPost("이게"));
   return (
     <PageContainer
       variants={pageEffect}
       initial="hidden"
       animate="visible"
-      ismobile={'' + isMobile}
+      ismobile={"" + isMobile}
     >
       <Input
         id="postSearch"
