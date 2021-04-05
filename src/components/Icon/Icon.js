10c446780b5bc8c451645ff48c6da0f6c71d28e8
Min Gee Suh <suh@student.unimelb.edu.au>
2021-04-05 10:43:05 +0900

    feat: changed navbar active style + new navbar icon

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,42 +1,71 @@
 import React from "react";
 import { string, oneOf } from "prop-types";
-import search from "assets/NavIcon/Search.svg";
-import home from "assets/NavIcon/Home.svg";
-import post from "assets/NavIcon/Post.svg";
-import profile from "assets/NavIcon/Profile.svg";
-import more from "assets/NavIcon/More.svg";
+import { ReactComponent as Search } from "./images/search.svg";
+import { ReactComponent as SearchActive } from "./images/search-active.svg";
+import { ReactComponent as Home } from "./images/home.svg";
+import { ReactComponent as HomeActive } from "./images/home-active.svg";
+import { ReactComponent as Post } from "./images/post.svg";
+import { ReactComponent as PostActive } from "./images/post-active.svg";
+import { ReactComponent as Profile } from "./images/profile.svg";
+import { ReactComponent as ProfileActive } from "./images/profile-active.svg";
+import { ReactComponent as Info } from "./images/info.svg";
+import { ReactComponent as InfoActive } from "./images/info-active.svg";
 
 /* -------------------------------------------------------------------------- */
 
-const Icon = ({ type, alt = "", height, style, ...restProps }) => {
-  let src = "";
-  const customStyle = { height, ...style };
+const Icon = ({ type, title = type, height = "25px", style, ...restProps }) => {
+  let COMP = null;
   switch (type) {
     case "search":
-      src = search;
+      COMP = Search;
+      break;
+    case "search-active":
+      COMP = SearchActive;
       break;
     case "home":
-      src = home;
+      COMP = Home;
+      break;
+    case "home-active":
+      COMP = HomeActive;
       break;
     case "post":
-      src = post;
+      COMP = Post;
+      break;
+    case "post-active":
+      COMP = PostActive;
       break;
     case "profile":
-      src = profile;
+      COMP = Profile;
       break;
-    case "more":
-      src = more;
+    case "profile-active":
+      COMP = ProfileActive;
+      break;
+    case "info":
+      COMP = Info;
+      break;
+    case "info-active":
+      COMP = InfoActive;
       break;
     default:
       throw new Error("아이콘을 찾을수없습니다");
   }
 
-  return <img src={src} alt={alt} style={customStyle} {...restProps} />;
+  return <COMP title={title} height={height} {...restProps} />;
 };
 
 Icon.propTypes = {
-  type: oneOf(["search", "home", "post", "profile", "more"]).isRequired,
-  alt: string,
+  type: oneOf([
+    "search",
+    "home",
+    "post",
+    "profile",
+    "info",
+    "profile-active",
+    "info-active",
+    "post-active",
+    "home-active",
+    "search-active",
+  ]).isRequired,
 };
 
 export default Icon;
--- src/containers/Nav/Navigation.js
+++ src/containers/Nav/Navigation.js
@@ -1,51 +1,33 @@
 import React from "react";
-import { Navbar, MobileNavbar } from "./Navigation.styled";
+import { Navbar } from "./Navigation.styled";
 import Icon from "components/Icon/Icon";
 import useDetectViewport from "hooks/useDetectViewport";
-// import { navlist } from "./Navigation.module.scss";
+import { useLocation } from "react-router";
 
 const Navigation = () => {
-  const { isMobile, isDesktop } = useDetectViewport();
+  const location = useLocation();
+  const { pathname } = location;
+  const { isMobile } = useDetectViewport();
   return (
     <Navbar center={isMobile}>
       <Navbar.Logo>Uninstagram</Navbar.Logo>
-      {isDesktop ? (
-        <Navbar.Items>
-          <Navbar.ItemLink exact to="/">
-            <Icon type="home" />
-          </Navbar.ItemLink>
-          <Navbar.ItemLink to="/search">
-            <Icon type="search" />
-          </Navbar.ItemLink>
-          <Navbar.ItemLink to="/post">
-            <Icon type="post" />
-          </Navbar.ItemLink>
-          <Navbar.ItemLink to="/profile">
-            <Icon type="profile" />
-          </Navbar.ItemLink>
-          <Navbar.ItemLink to="/more">
-            <Icon type="more" />
-          </Navbar.ItemLink>
-        </Navbar.Items>
-      ) : (
-        <MobileNavbar.Items>
-          <MobileNavbar.ItemLink exact to="/">
-            <Icon type="home" />
-          </MobileNavbar.ItemLink>
-          <MobileNavbar.ItemLink to="/search">
-            <Icon type="search" />
-          </MobileNavbar.ItemLink>
-          <MobileNavbar.ItemLink to="/post">
-            <Icon type="post" />
-          </MobileNavbar.ItemLink>
-          <MobileNavbar.ItemLink to="/profile">
-            <Icon type="profile" />
-          </MobileNavbar.ItemLink>
-          <MobileNavbar.ItemLink to="/more">
-            <Icon type="more" />
-          </MobileNavbar.ItemLink>
-        </MobileNavbar.Items>
-      )}
+      <Navbar.Items isMobile={isMobile}>
+        <Navbar.ItemLink exact to="/">
+          <Icon type={pathname === "/" ? "home-active" : "home"} />
+        </Navbar.ItemLink>
+        <Navbar.ItemLink to="/search">
+          <Icon type={pathname === "/search" ? "search-active" : "search"} />
+        </Navbar.ItemLink>
+        <Navbar.ItemLink to="/post">
+          <Icon type={pathname === "/post" ? "post-active" : "post"} />
+        </Navbar.ItemLink>
+        <Navbar.ItemLink to="/profile">
+          <Icon type={pathname === "/profile" ? "profile-active" : "profile"} />
+        </Navbar.ItemLink>
+        <Navbar.ItemLink to="/info">
+          <Icon type={pathname === "/info" ? "info-active" : "info"} />
+        </Navbar.ItemLink>
+      </Navbar.Items>
     </Navbar>
   );
 };
